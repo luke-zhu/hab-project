3abb7b4a11dd3781d7410f1b998f642368c7a50f
luke-zhu <luke_zhu@brown.edu>
2017-02-04 16:27:53 -0500

    First commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+// import { createContainer } from 'meteor/react-meteor-data'
+
+
+const App = () => (
+  <div>
+    <AppBar
+      title="Hello!"
+      showMenuIconButton={false}
+    />
+    <Paper style={{ padding: 10 }} zDepth={2}>
+      <h3>Project Outline</h3>
+      <h4>Overview</h4>
+      <p>
+      This project takes in a search query, finds top 10 products on amazon,
+      translates the names of these products into some language, finds
+      alternatives on foreign sites, and displays information about these items
+      in an interesting way.
+      </p>
+      <h4>Technology</h4>
+      <p>Tensorflow, currently using a linear model that can be scaled to a
+      deep learning model</p>
+      <h4>Market</h4>
+      <p>People interested in getting items for a lower price, or selling items
+      in the US for a profit</p>
+      <h4>Products</h4>
+      <p>Generally items that people are willing to buy for a cheaper price from
+      an unknown seller. An item that is used for a short time and is non-essential.
+      </p>
+      <TextField hintText="SEARCH FOR SOMETHING" />
+    </Paper>
+  </div>
+);
+
+export default App;
---
+++ imports/ui/redux/reducers.js
@@ -0,0 +1,11 @@
+const initialState = {
+};
+
+const reducers = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducers;
