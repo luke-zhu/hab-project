baccb7320a438e032867d8810b4ba2e3c09efc18
Andrew Kim <therladbsgh@gmail.com>
2017-02-04 19:36:57 -0500

    Add translation functionality

--- imports/api/search.js
+++ imports/api/search.js
@@ -5,6 +5,15 @@ Meteor.methods({
   'search.filterTitle': (title) => {
     const words = title.split(' ');
     const unique = words.filter((e, i) => i === words.indexOf(e));
-    return 
+    return
+  },
+
+  'translate-chinese-simplified': function(titleVar){
+    const translate = require('google-translate-api');
+    translate(titleVar, {to: 'zh-cn'}).then(res => {
+      console.log(res.text);
+    }).catch(err => {
+      console.error(err);
+    });
   },
 });
